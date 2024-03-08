b63c54866405bc3b61554a888ce801afa239c2f1
suraj7974 <surajpatelsuraj55@gmail.com>
2024-03-08 21:40:42 +0530

    now black pieces can also move and no other piece can take over any piece if put over them

--- src/referee/referee.ts
+++ src/referee/referee.ts
@@ -1,23 +1,41 @@
-import { PieceType, TeamType } from "../components/chessboard/Chessboard";
+import {
+  PieceType,
+  TeamType,
+  Piece,
+} from "../components/chessboard/Chessboard";
 
 export default class Referee {
+  tileIsOccupied(x: number, y: number, boardState: Piece[]): boolean {
+    const piece= boardState.find((p)=>p.x==x&&p.y==y);
+    if(piece){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
   isValidMove(
     px: number,
     py: number,
     x: number,
     y: number,
     type: PieceType,
-    team: TeamType
+    team: TeamType,
+    boardState: Piece[]
   ) {
     if (type === PieceType.PAWN) {
       if (team === TeamType.OUR) {
         if (py === 1) {
           if (px === x && (y - py === 1 || y - py === 2)) {
-            return true;
+            if (!this.tileIsOccupied(x, y, boardState)) {
+              return true;
+            }
           }
         } else {
           if (px === x && y - py === 1) {
-            return true;
+            if (!this.tileIsOccupied(x, y, boardState)) {
+              return true;
+            }
           }
         }
       } else {
@@ -25,8 +43,8 @@ export default class Referee {
           if (px === x && (y - py === -1 || y - py === -2)) {
             return true;
           }
-        }else{
-          if(px===x && y-py===-1){
+        } else {
+          if (px === x && y - py === -1) {
             return true;
           }
         }
