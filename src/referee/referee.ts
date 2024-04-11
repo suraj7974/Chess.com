5a991d8dc66fa53fd8ffc24dedba76ca15d0441e
suraj7974 <surajpatelsuraj55@gmail.com>
2024-04-11 22:14:23 +0530

    added a multiplier variable for refractoring code in horizontal and vertical movements

--- src/referee/referee.ts
+++ src/referee/referee.ts
@@ -167,15 +167,35 @@ export default class Referee {
     }
     if (type === PieceType.ROOK) {
       //movement
-      for (let i = 1; i < 8; i++) {
-        if (desiredPosition.x - initialPosition.x === -i) {
-          return true;
-        } else if (desiredPosition.x - initialPosition.x === i) {
-          return true;
-        } else if (desiredPosition.y - initialPosition.y === i) {
-          return true;
-        } else if (desiredPosition.y - initialPosition.y === -i) {
-          return true;
+
+      //horizontal
+      if (initialPosition.y === desiredPosition.y) {
+        for (let i = 1; i < 8; i++) {
+          let multiplier = desiredPosition.x < initialPosition.x ? -1 : 1;
+          let passedPosition: Position = {
+            x: initialPosition.x + i * multiplier,
+            y: initialPosition.y,
+          };
+          if (passedPosition.x === desiredPosition.x && passedPosition.y === desiredPosition.y) {
+            console.log("arrived");
+            break;
+          }
+          // return true;
+        }
+      }
+      //vertical
+      if (initialPosition.x === desiredPosition.x) {
+        for (let i = 1; i < 8; i++) {
+          let multiplier = desiredPosition.y < initialPosition.y ? -1 : 1;
+          let passedPosition: Position = {
+            x: initialPosition.x,
+            y: initialPosition.y + i * multiplier,
+          };
+          if (passedPosition.x === desiredPosition.x && passedPosition.y === desiredPosition.y) {
+            console.log("arrived");
+            break;
+          }
+          // return true;
         }
       }
     }
